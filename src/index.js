5ae873c3f5ccf1c6c6c9aecb0168187c867e5659
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2023-09-03 16:02:17 +0300

    develop: added header to index.html, content, select, category, content image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './scss/styles.scss';
+
+import Choices from 'choices.js'
+
+const dropdownHandler = (event) => {
+    if (event.target.classList.contains('header__mobile')) {
+        const dropdown = event.target.querySelector('.header__mobile-dropdown');
+        if (!dropdown.classList.contains('active')) {
+            dropdown.classList.add('active');
+        } else {
+            dropdown.classList.remove('active');
+        }
+    }
+}
+
+if (document.querySelector('.select__category')) {
+    const select = document.querySelector('.select__category');
+    const choices = new Choices(select, {
+        loadingText: 'Загрузка...',
+        noResultsText: 'Не найдено результатов поиска.',
+        itemSelectText: 'Нажми для выбора'
+    });
+}
+
+if (document.querySelector('.header__mobile')) {
+    document.querySelector('.header__mobile').addEventListener('click', dropdownHandler)
+}
